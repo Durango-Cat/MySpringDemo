10e8e4c78f050e322711928208c54829da6b0e20
猪猪女孩 <13429761558@163.com>
2019-04-01 18:09:41 +0800

    完善希尔排序、插入排序、选择排序的代码，并提交归并排序（自顶向下、自底向上）

---
+++ src/main/java/sort/MergerSort.java
@@ -0,0 +1,137 @@
+package main.java.sort;
+
+import org.junit.Test;
+
+/**
+ * 归并排序(属于稳定排序）
+ * 原理：将数组复制一份大小相同的数组，然后每次归并回原来的数组中，
+ *      归并过程：从中间分成2组，继续从中间分成2组，知道分到粒度为1（不能再分的时候）进行比较元素，在每次归并的时候，会先进行找中间元素，
+ *      找到中间元素后进行左右划分。
+ *      调用左右两边的归并方法，归并结束后继续合并元素。
+ *
+ * @author Zhuqiuping on 2019/3/29
+ */
+public class MergerSort {
+
+    private static Double[] aux;
+
+    private Double[] a = new Double[]{23.43D, 23232.23D, 34.23D, 1D, 14.2D, 20.343D, 20.34D, 20.3D, 22.32D, 34.23D, 23.23D};
+
+    /**
+     * 归并排序的自顶向下排序测试
+     */
+    @Test
+    public void testTopDown() {
+        System.out.println("自顶向下排序前：");
+        for(int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + "\t");
+        }
+        sortTopDown(a);
+        System.out.println();
+        System.out.println("排序后：");
+        for(int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + "\t");
+        }
+    }
+
+    /**
+     * 归并排序的自底向上排序测试
+     */
+    @Test
+    public void testDownToTop() {
+        System.out.println("自底向上排序前：");
+        for(int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + "\t");
+        }
+        sortDownToTop(a);
+        System.out.println();
+        System.out.println("排序后：");
+        for(int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + "\t");
+        }
+    }
+
+    /**
+     * 归并排序 自底向上
+     */
+    public void sortDownToTop(Double[] a) {
+        int size = a.length;
+        aux = new Double[size];
+        System.arraycopy(a, 0, aux, 0, size);
+        //记不住排序，就在关键的地方多备注点，这些排序运行前面的排序数值多些，mid不一定真的就是中间的，只要保证分出来mid前后元素都已经排过序了
+        //此处的排序增加粒度，是每次2的幂次方，2的0次幂，
+        for(int lowSize = 1; lowSize < size; lowSize = lowSize * 2) {
+            //此处是每次循环粒度下的，小循环范围
+            for(int low = 0; low < size - lowSize; low += lowSize * 2) {
+                merge(a, low, low + lowSize - 1, Math.min(low + lowSize * 2 - 1, size - 1));
+            }
+        }
+    }
+
+    /**
+     * 归并排序 自顶向下
+     */
+    public void sortTopDown(Double[] a) {
+        aux = new Double[a.length];
+        System.arraycopy(a, 0, aux, 0, a.length);
+        //这块的数值要做到数组长度-1，如果是数组的长度，后面merge里面
+        sortTopDown(a, 0, a.length - 1);
+    }
+
+    /**
+     * 归并排序 自顶向下
+     *
+     * @param a         待排序数组
+     * @param low       低位
+     * @param high      高位
+     */
+    public void sortTopDown(Double[] a, int low, int high) {
+        int mid = low + (high - low) / 2;
+        //这里面应该有个截止的条件
+        if(high < 0 || low < 0 || high <= low || mid >= high) {
+            return;
+        }
+        //此处也要注意：前面回调方法的结束参数值应该是mid,包含的数值多点，这样能多比较些，可能会出现3个数值时，前面的回调函数比较了前两个数值，
+        // 保证了要比较的数组中前面的数值一定是有序的
+        sortTopDown(a, low, mid);
+        if(mid != low) {
+            sortTopDown(a, mid + 1, high);
+        }
+        merge(a, low, mid, high);
+    }
+
+    /**
+     * 原地归并
+     *
+     * @param a             原数组
+     * @param low           低位索引
+     * @param mid           中间位置
+     * @param high          高位索引
+     */
+    public void merge(Double[] a, int low, int mid, int high) {
+        int i = low, j = mid + 1;
+
+        for(int k = low; k <= high; k++) {
+            aux[k] = a[k];
+        }
+
+        for(int k = low; k <= high; k++) {
+            //左边元素已经合并完了
+            if(i > mid) {
+                a[k] = aux[j++];
+            }
+            //右边元素已经合并完了
+            else if(j > high) {
+               a[k] = aux[i++];
+            }
+            //左边元素未合并完
+            //用未排序的数组来进行数值比较，要将j++这个判断放在前面，因为归并排序是稳定的数组，
+            // 要做到稳定就要将数组前面与后面数值相同的时候，将前面的值先填充到数组中
+            else if(aux[j] < aux[i]) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+}
--- src/main/java/sort/SortCompareTest.java
+++ src/main/java/sort/SortCompareTest.java
@@ -1,10 +1,5 @@
 package main.java.sort;
 
-import com.google.common.collect.Lists;
-import org.junit.Test;
-
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
 /**
@@ -15,65 +10,152 @@ import java.util.Random;
  */
 public class SortCompareTest {
 
-    public static double time(String alg, Double[] a) {
+    private static String selectionSort = "Selection sort", insertionSort = "insertion sort", shellSort = "shell sort";
+    public static Long time(String alg, Double[] a) {
         if(alg == null || alg.isEmpty()) {
-            return 0.0;
+            return 0L;
         }
 
-        if("Selection sort".equals(alg)) {
-
+        int size = a.length;
+        for(int i = 0; i < size; i++) {
+            System.out.print(a[i] + "\t");
         }
-        return 0;
+        System.out.println();
+        Long startTime = System.currentTimeMillis();
+        if(selectionSort.equals(alg)) {
+            a = selection(a);
+        } else if(insertionSort.equals(alg)) {
+            a = insertion(a);
+        } else if(shellSort.equals(alg)) {
+            a = shell(a);
+        }
+        Long endTime = System.currentTimeMillis();
+        for(int i = 0; i < size; i++) {
+            System.out.print(a[i] + "\t");
+        }
+        return endTime - startTime;
     }
 
-    public static double timeRandomInput(String alg, int N, int T) {
-        double total = 0.0;
+    /**
+     * 选择排序（从小到大）
+     * 思路：从第一个元素开始遍历集合，找到最小的值，把索引跟第一个索引下的值换下
+     *      接下来从第二个元素开始继续遍历，找到剩下值里的最小值，将索引跟第二个索引下的值换下
+     *
+     * @param a 待排序的集合
+     * @return 已经排好序的集合
+     */
+    public static Double[] selection(Double[] a) {
+
+        int size = a.length;
+
+        for(int i = 0; i < size; i++) {
+            //最小元素的索引
+            int min = i;
+            for(int j = i+1; j < size; j++) {
+                if(a[min] > a[j]) {
+                    min = j;
+                }
+            }
+            //找到最小值
+            if(i != min) {
+                double temp = a[min];
+                a[min] = a[i];
+                a[i] = temp;
+            }
+        }
+
+        return a;
+    }
+
+    /**
+     * 插入排序（从小到大）
+     * 思路：从第二个元素开始向左遍历元素，比较左边元素跟当前元素大小，如果大于当前元素，就交换。
+     * 本来插入排序的规则是：从左到右开始，遍历的左边元素都是有序的，如果该元素小于左边元素，就找到合适的位置让该位置的元素一直到都向右移一位。
+     *
+     * @param a 待排序的集合
+     * @return 已经排好序的集合
+     */
+    public static Double[] insertion(Double[] a) {
+        int size = a.length;
+        for(int i = 1; i < size; i++) {
+            for(int j = i; j > 0 && (a[j] < a[j - 1]); j--) {
+                each(j, j - 1, a);
+            }
+        }
+        return a;
+    }
+
+    /**
+     * 希尔排序（从小到大）
+     *
+     * 思路：按照数组长度除以3+1之后的值，即（1、4、7...)的宽度，按照这个宽度比较这个宽度内的数值
+     * 比如9位长的，第一次是从4的索引上比较索引4前4个的元素大小，如果小于就互换。如果不小于，就继续比较索引为5的
+     *
+     * @param a 待排序的集合
+     * @return 已经排好序的集合
+     */
+    public static Double[] shell(Double[] a) {
+        int size = a.length;
+        int h = 1;
+        while(h < size / 3) {
+            h = h * 3 + 1;
+        }
+        while (h >= 1) {
+            for (int i = h; i < size; i++) {
+                for (int j = i; j >= h && a[j] < a[j - h]; j -= h) {
+                    each(j - h, j, a);
+                }
+            }
+            h *= 3;
+        }
+        return a;
+    }
+
+    /**
+     * 随机数组并统计出来每个排序花费的时间
+     *
+     * @param alg
+     * @param N
+     * @param T
+     * @return
+     */
+    public static Long[] timeRandomInput(String alg, int N, int T) {
+        Long[] total = new Long[T];
         Double[] a = new Double[N];
         Random random = new Random();
 
         for(int t = 0; t < T; t++) {
             //每一次循环都会对该数组进行一次赋值和排序
             for(int i = 0; i < N; i++) {
-                a[i] = random.nextDouble();
+                a[i] = random.nextDouble() * N;
             }
-            total += time(alg, a);
+            total[t] = time(alg, a);
         }
         return total;
     }
 
-    @Test
-    public void testListString() {
-        List<String> weekList = Lists.newArrayList();
-        weekList.add("analyzier_2018-04");
-        weekList.add("analyzier_2018-03");
-        weekList.add("analyzier_2018-05");
-        weekList.add("analyzier_2018-06");
-        weekList.add("analyzier_2018-02");
-        weekList.add("analyzier_2018-01");
-        weekList.add("analyzier_2017-11");
-        weekList.add("analyzier_2017-12");
+    public static void main(String[] args) {
+        Long[] insertSortTime = timeRandomInput(insertionSort, 16, 5);
+        Long[] selectionSortTime = timeRandomInput(selectionSort, 16, 5);
+        Long[] shellSortTime = timeRandomInput(shellSort, 16, 5);
 
-        weekList.forEach(System.out::println);
-        Collections.sort(weekList);
-
-        weekList.forEach(System.out::println);
+        for(int i = 0; i < 5; i++) {
+            System.out.println(insertSortTime[i] + "插入排序的第" +(i+1) +"个花费的时间");
+            System.out.println(selectionSortTime[i] + "选择排序的第" +(i+1) +"个花费的时间");
+            System.out.println(shellSortTime[i] + "希尔排序的第" +(i+1) +"个花费的时间");
+        }
     }
 
-    @Test
-    public void testList() {
-        List<String> stringList = Lists.newArrayList("@猪猪", "乐乐", "豆豆", "豆豆", "乐乐", "@猪猪");
-        stringList.forEach(System.out::print);
-        System.out.println();
-        stringList.remove("@猪猪");
-        stringList.forEach(System.out::print);
-    }
-
-    @Test
-    public void testString() {
-        String str = "_trans_ref.TT";
-        //System.out.println(".".contains(str));
-        System.out.println(str.startsWith("-"));
-        System.out.println(str.startsWith("0x", 1));
-        System.out.println(str.startsWith("#", 0));
+    /**
+     * 交换元素
+     *
+     * @param left  左边的元素
+     * @param right 右边的元素
+     * @param a     数组
+     */
+    private static void each(int left, int right, Double[] a) {
+        Double temp = a[left];
+        a[left] = a[right];
+        a[right] = temp;
     }
 }
