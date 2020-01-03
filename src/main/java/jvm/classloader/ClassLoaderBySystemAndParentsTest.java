488e32148d597dd37f8ee37a949b83fb0151ad84
zhuqp <13429761558@163.com>
2020-01-03 16:20:57 +0800

    [jvm]: 对类加载器的调用场景补充，比如Thread.currentThread.getContextClassLoader等方式

---
+++ src/main/java/jvm/classloader/ClassLoaderBySystemAndParentsTest.java
@@ -0,0 +1,28 @@
+package main.java.jvm.classloader;
+
+/**
+ * 测试当前类的系统类加载器，以及系统类加载器的父类加载器信息
+ *
+ * 打印结果：
+ * sun.misc.Launcher$AppClassLoader@18b4aac2
+ * sun.misc.Launcher$ExtClassLoader@452b3a41
+ * null
+ * @author Zhuqiuping on 2020/1/3
+ */
+public class ClassLoaderBySystemAndParentsTest {
+
+    public static void main(String[] args) {
+        //得到系统类加载器
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+        System.out.println(classLoader);
+
+        while(classLoader != null) {
+            //得到系统类加载器的父类加载器
+            classLoader = classLoader.getParent();
+            //如果是根类加载器 就直接返回null, 可以在ClassLoader类中能找到对应的getParent()
+            System.out.println(classLoader);
+        }
+    }
+}
+
---
+++ src/main/java/jvm/classloader/ThreadClassLoaderTest.java
@@ -0,0 +1,40 @@
+package main.java.jvm.classloader;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
+/**
+ * 线程中上下文的加载器
+ *
+ * @author Zhuqiuping on 2020/1/3
+ */
+public class ThreadClassLoaderTest {
+
+    public static void main(String[] args) throws IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        System.out.println(classLoader);
+
+        String resourceClassName = "main/java/jvm/classloader/ClassLoaderBySystemAndParentsTest.class";
+
+        Enumeration<URL> resources = classLoader.getResources(resourceClassName);
+        while(resources.hasMoreElements()) {
+            URL url = resources.nextElement();
+            System.out.println(url);
+        }
+
+        System.out.println("------");
+        //在不同类型的class文件 所用到的类加载器也是不一样的
+        //比如目前这个类 就是用到了系统类加载器（应用类加载器）   如果是String类的class 在加载的时候是用的根类加载器
+        Class<ThreadClassLoaderTest> threadClassLoaderTestClass = ThreadClassLoaderTest.class;
+        ClassLoader classLoader1 = threadClassLoaderTestClass.getClassLoader();
+        System.out.println(classLoader1);
+
+        System.out.println("------");
+
+        Class<String> stringClass = String.class;
+        ClassLoader classLoader2 = stringClass.getClassLoader();
+        System.out.println(classLoader2);
+    }
+}
